359daf8c34a362e3bd7f12c0d00e73649a33ce1f
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-05-01 00:59:19 +0800

    ♻️ refactor: Modify main.dart to handle multiple message type request

--- functions/messaging/lib/main.dart
+++ functions/messaging/lib/main.dart
@@ -1,34 +1,56 @@
 import 'dart:async';
-import 'package:dart_appwrite/dart_appwrite.dart';
+import 'dart:convert';
+import 'dart:io';
 
-// This is your Appwrite function
-// It's executed each time we get a request
+import 'package:messaging_function/account_creation_message.dart';
+import 'package:messaging_function/account_validation_message.dart';
+import 'package:messaging_function/delete_account_message.dart';
+import 'package:messaging_function/new_patient_message.dart';
+import 'package:messaging_function/new_remarks_message.dart';
+import 'package:messaging_function/password_reset_message.dart';
+
+// Appwrite Client environment variables
+final String projectEndpoint = Platform.environment["APPWRITE_ENDPOINT"]!;
+final String projectID = Platform.environment["APPWRITE_PROJECT"]!;
+final String api = Platform.environment["API"]!;
+
+final String databaseID = Platform.environment["DATABASE"]!;
+final String usersCollection = Platform.environment["USER_COLLECTION"]!;
+final String schoolCollection = Platform.environment["SCHOOL_COLLECTION"]!;
+final String assignmentCollection = Platform.environment["ASSIGNMENT_COLLECTION"]!;
+
+final String adminId = Platform.environment["ADMIN_ID"]!;
+
 Future<dynamic> main(final context) async {
-// Why not try the Appwrite SDK?
-  //
-  // final client = Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(Platform.environment['APPWRITE_FUNCTION_PROJECT_ID'])
-  //    .setKey(Platform.environment['APPWRITE_API_KEY']);
+  context.log("Decoding body to get message type...");
+  final body = json.decode(context.req.bodyRaw);
+  final response = body['message_type'] as String;
 
-  // You can log messages to the console
-  context.log('Hello, Logs!');
-
-  // If something goes wrong, log an error
-  context.error('Hello, Errors!');
-
-  // The `req` object contains the request data
-  if (context.req.method == 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return context.res.send('Hello, World!');
+  try {
+    if (response == "request_account_deletion") {
+      accountDeletion(context);
+    } else if (response == "account_creation") {
+      accountCreation(context);
+    } else if (response == "account_validation") {
+      accountValidation(context);
+    } else if (response == "new_remark") {
+      newRemarks(context);
+    } else if (response == "new_patient") {
+      newPatient(context);
+    } else if (response == "password_reset") {
+      passwordReset(context);
+    // } else if (response == "new_screening") {
+    // } else if (response == "patient_refer") {
+    // } else if (response == "mfa_enabled") {
+    // } else if (response == "mfa_disabled") {
+    } else {
+      return context.res.json({
+        "error": "Invalid message type",
+      });
+    }
+  } catch (e) {
+    return context.res.json({
+      "error": e.toString(),
+    });
   }
-
-  // `res.json()` is a handy helper for sending JSON
-  return context.res.json({
-    'motto': 'Build like a team of hundreds_',
-    'learn': 'https://appwrite.io/docs',
-    'connect': 'https://appwrite.io/discord',
-    'getInspired': 'https://builtwith.appwrite.io',
-  });
 }
