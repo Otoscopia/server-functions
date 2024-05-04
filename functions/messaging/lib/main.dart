7771e2e781a25db887580b443f6065a1116cec15
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-05-04 15:18:39 +0800

    ♻️ refactor: Move messages function to separate folder and use function index to get all messaging functions

--- functions/messaging/lib/main.dart
+++ functions/messaging/lib/main.dart
@@ -1,23 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
 
-import 'package:messaging_function/account_creation_message.dart';
-import 'package:messaging_function/account_validation_message.dart';
-import 'package:messaging_function/delete_account_message.dart';
-import 'package:messaging_function/new_patient_message.dart';
-import 'package:messaging_function/new_remarks_message.dart';
-import 'package:messaging_function/password_reset_message.dart';
-
-// Appwrite Client environment variables
-final String projectEndpoint = Platform.environment["APPWRITE_ENDPOINT"]!;
-final String projectID = Platform.environment["APPWRITE_PROJECT"]!;
-final String api = Platform.environment["API"]!;
-
-final String databaseID = Platform.environment["DATABASE"]!;
-final String usersCollection = Platform.environment["USER_COLLECTION"]!;
-
-final String adminId = Platform.environment["ADMIN_ID"]!;
+import 'package:messaging_function/message_functions/index.dart';
 
 Future<dynamic> main(final context) async {
   context.log("Decoding body to get message type...");
@@ -37,15 +21,19 @@ Future<dynamic> main(final context) async {
       newPatient(context);
     } else if (response == "password_reset") {
       passwordReset(context);
-    // } else if (response == "new_screening") {
-    // } else if (response == "patient_refer") {
-    // } else if (response == "mfa_enabled") {
-    // } else if (response == "mfa_disabled") {
+      // } else if (response == "new_screening") {
+      // } else if (response == "patient_refer") {
+      // } else if (response == "mfa_enabled") {
+      // } else if (response == "mfa_disabled") {
     } else {
       return context.res.json({
         "error": "Invalid message type",
       });
     }
+
+    return context.res.json({
+      "data": "Function messaging created and sent emails successfully!",
+    });
   } catch (e) {
     return context.res.json({
       "error": e.toString(),
