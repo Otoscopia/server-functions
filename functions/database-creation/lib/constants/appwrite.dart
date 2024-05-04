ec0c1e858e3b89b8b1f44e0f4c4431d9f3f8bbca
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-05-04 19:18:40 +0800

    ✨ feat: Add Appwrite constants for project endpoint, project ID, and API usage

---
+++ functions/database-creation/lib/constants/appwrite.dart
@@ -0,0 +1,5 @@
+import 'dart:io';
+
+final String projectEndpoint = Platform.environment["APPWRITE_ENDPOINT"]!;
+final String projectID = Platform.environment["APPWRITE_PROJECT"]!;
+final String api = Platform.environment["API"]!;
