c22133bb6fecc1360fcb60ef84281a6029c4f97e
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2024-05-04 19:26:56 +0800

    ♻️ refactor: Add function_type for handling database modification

--- functions/database-creation/lib/main.dart
+++ functions/database-creation/lib/main.dart
@@ -1,35 +1,29 @@
 import 'dart:async';
+import 'dart:convert';
 
-import 'package:dart_appwrite/dart_appwrite.dart';
+import 'package:database_creation/functions/attribute_creation.dart';
+import 'package:database_creation/functions/functions.dart';
 
-// This is your Appwrite function
-// It's executed each time we get a request
 Future<dynamic> main(final context) async {
-  // Why not try the Appwrite SDK?
-  //
-  // final client = Client()
-  //   .setEndpoint('https://cloud.appwrite.io/v1')
-  //   .setProject(Platform.environment['APPWRITE_FUNCTION_PROJECT_ID'])
-  //   .setKey(Platform.environment['APPWRITE_API_KEY']);
+  try {
+    context.log("Decoding body...");
+    final body = json.decode(context.req.bodyRaw);
+    final type = body['function_type'] as String;
 
-  // You can log messages to the console
-  context.log('Hello, Logs!');
+    if (type == "database_creation") {
+      databaseCreation(context);
+    } else if (type == "collection_creation") {
+      collectionCreation(context);
+    } else if (type == "attribute_creation") {
+      attributeCreation(context);
+    }
 
-  // If something goes wrong, log an error
-  context.error('Hello, Errors!');
-
-  // The `req` object contains the request data
-  if (context.req.method == 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return context.res.send('Hello, World!');
+    return context.res.json({
+      'data': 'function database-creation run successfully!',
+    });
+  } catch (e) {
+    return context.res.json({
+      "error": e.toString(),
+    });
   }
-
-  // `res.json()` is a handy helper for sending JSON
-  return context.res.json({
-    'motto': 'Build like a team of hundreds_',
-    'learn': 'https://appwrite.io/docs',
-    'connect': 'https://appwrite.io/discord',
-    'getInspired': 'https://builtwith.appwrite.io',
-  });
 }
